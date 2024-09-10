383adfe6e4b5312fb3bcb3df8276aca0959bf6bb
DiogoTV <eukaua2017@gmail.com>
2024-09-10 18:38:51 -0300

    crash handler layout fixing

--- source/data/CrashHandler.hx
+++ source/data/CrashHandler.hx
@@ -1,5 +1,6 @@
 package data;
 
+import openfl.display.Stage;
 import openfl.display.BitmapData;
 import openfl.display.Bitmap;
 import openfl.Assets;
@@ -11,14 +12,18 @@ import openfl.display.Sprite;
 
 class CrashHandler extends Sprite
 {
+    var _stage:Stage;
+
     var imgData:BitmapData;
 
     public function new(stack:String, ?path:String = "")
     {
         super();
+        this._stage = openfl.Lib.application.window.stage;
+
         final _matrix = new flixel.math.FlxMatrix().rotateByPositive90();
 		graphics.beginGradientFill(LINEAR, [0xFF38173F, 0xFF733A8D], [0.6, 1], [125, 255], _matrix);
-        graphics.drawRect(0, 0, FlxG.width, FlxG.height);
+        graphics.drawRect(0, 0, _stage.stageWidth, _stage.stageHeight);
         graphics.endFill();
 
         var errorField = new TextField();
@@ -30,19 +35,19 @@ class CrashHandler extends Sprite
             );
             field.multiline = true;
             field.autoSize = CENTER;
-            field.width = FlxG.width;
-            field.x = (FlxG.width - field.width) / 2;
-            field.wordWrap = false;
+            field.width = _stage.stageWidth * 0.8;
+            field.x = (_stage.stageWidth - field.width) / 2;
+            field.selectable = false;
         }
         errorField.text = 'THE GAME HAS CRASHED\n\n${stack}\n\nCrash log created at: "${path}"';
         errorField.y = 24;
         pressField.text = 'Press ESCAPE to return to main menu\nPress ENTER to open github issues';
-        pressField.y = FlxG.height - pressField.height - 24;
+        pressField.y = _stage.stageHeight - pressField.height - 24;
 
         imgData = Assets.getBitmapData("assets/images/crash.png");
         var crashImg = new Bitmap(imgData);
-        crashImg.x = (FlxG.width - crashImg.width) / 2;
-        crashImg.y = (FlxG.height - crashImg.height - pressField.height - 32);
+        crashImg.x = (_stage.stageWidth - crashImg.width) / 2;
+        crashImg.y = (_stage.stageHeight- crashImg.height - pressField.height - 32);
 
         addChild(crashImg);
         addChild(errorField);
