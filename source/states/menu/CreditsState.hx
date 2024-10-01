077774e1e0d9a09ebf9e6d07d0a94156cf0b3f42
DiogoTV <eukaua2017@gmail.com>
2024-10-01 02:44:44 -0300

    yayy more people to credit

--- source/states/menu/CreditsState.hx
+++ source/states/menu/CreditsState.hx
@@ -71,19 +71,21 @@ class CreditsState extends MusicBeatState
         infoTxt.setBorderStyle(OUTLINE, 0xFF000000, 1.5);
         add(infoTxt);
 
-		final specialPeople = 'Anakim, ArturYoshi, BeastlyChip♧, Bnyu, Evandro, NxtVithor, Pi3tr0, Raphalitos, Yoisabo, ZieroSama';
-		final specialCoders = 'Crowplexus, Gazozoz';
+		final specialPeople = 'Anakim, ArturYoshi, BeastlyChip♧, Bnyu, Evandro, NxtVithor, Pi3tr0, Raphalitos, ZieroSama <33';
+		final specialCoders = 'Teles, Crowplexus, Gazozoz';
 		// yes, this implies coders aren't people
 		// :D
 		
 		// btw you dont need to credit everyone here on your mod, just credit doido engine as a whole and we're good
-		addCredit('DiogoTV', 			'diogotv', 	0xFFC385FF, "Doido Engine's Owner and Main Coder", 			'https://twitter.com/DiogoTVV');
-		addCredit('teles', 				'teles', 	0xFFFF95AC, "Doido Engine's Additional Coder",					'https://youtube.com/@telesfnf');
-		addCredit('Anna The Fennec',	'anna', 	0xFFFFE100, "Main designer of Doido Engine's chart editor",	'https://twitter.com/goldenfoxy2604');
-		addCredit('JulianoBeta', 		'juyko', 	0xFF0BA5FF, "Composed Doido Engine's offset menu music",		'https://www.youtube.com/@prodjuyko');
-		addCredit('BeastlyNikoo', 		'nikoo', 	0xFF60458A, "(WIP) Logo Artist",								'https://twitter.com/Mudoku__');
-		addCredit('Github Contributors','github', 	0xFFFFFFFF, 'Thank you\n${specialCoders}!!', 'https://github.com/DoidoTeam/FNF-Doido-Engine/graphs/contributors');
-		addCredit('Special Thanks', 	'heart', 	0xFFC01B42, 'Thank you\n${specialPeople}!!', "https://youtu.be/rnr8Oo2RV_c");
+		addCredit('DiogoTV', 			'diogotv', 	 0xFFC385FF, "Doido Engine's Owner and Main Coder", 				'https://bsky.app/profile/diogotv.bsky.social');
+		addCredit('teles', 				'teles', 	 0xFFFF95AC, "Doido Engine's Additional Coder",					'https://youtube.com/@telesfnf');
+		addCredit('Anna The Fennec',	'anna', 	 0xFFFFE100, "Main designer of Doido Engine's chart editor",		'https://bsky.app/profile/goldenfoxy.bsky.social');
+		addCredit('JulianoBeta', 		'juyko', 	 0xFF0BA5FF, "Composed Doido Engine's offset menu music",			'https://www.youtube.com/@prodjuyko');
+		addCredit('CrowPlexus',			'crowplexus',0xFF292C2E, "Creator of HScript Iris",							'https://github.com/crowplexus/hscript-iris');
+		addCredit('yoisabo',			'yoisabo',	 0xFF56EF19, "Chart Editor's Event Icons Artist",					'https://bsky.app/profile/yoisabo.bsky.social');
+		addCredit('doubleonikoo', 		'nikoo', 	 0xFF60458A, "is gonna make an actual logo for Doido Engine soon",	'https://bsky.app/profile/doubleonikoo.bsky.social');
+		addCredit('Github Contributors','github', 	 0xFFFFFFFF, 'Thank you\n${specialCoders}!!', 'https://github.com/DoidoTeam/FNF-Doido-Engine/graphs/contributors');
+		addCredit('Special Thanks', 	'heart', 	 0xFFC01B42, 'Thank you\n${specialPeople}!!', "https://youtu.be/rnr8Oo2RV_c");
 		
 		for(i in 0...creditList.length)
 		{
