2e165109e2ec341bf19a9bef1f322a5ae50a5c4d
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-10-15 17:48:21 +0300

    yeap, now no more resetState on desktop

--- source/subStates/OffsetsSubState.hx
+++ source/subStates/OffsetsSubState.hx
@@ -228,7 +228,11 @@ class OffsetsSubState extends MusicBeatSubState
                 offsetMusic.stop();
                 if(FlxG.sound.music != null)
                     FlxG.sound.music.play();
+                #if mobile
                 FlxG.resetState();
+                #else
+                close();
+                #end
             }
         
             if(Controls.justPressed(UI_UP))
