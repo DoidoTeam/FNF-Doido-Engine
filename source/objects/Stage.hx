0bf79ffcef8846f295f395f1fdea9174d117b1db
teles <85184617+iamteles@users.noreply.github.com>
2024-11-30 19:49:49 +0000

    more stage updates

--- source/objects/Stage.hx
+++ source/objects/Stage.hx
@@ -13,6 +13,7 @@ class Stage extends FlxGroup
 
 	public var curStage:String = "";
 	public var gfVersion:String = "no-gf";
+	public var camZoom:Float = 1;
 
 	// things to help your stage get better
 	public var bfPos:FlxPoint  = new FlxPoint();
@@ -61,19 +62,40 @@ class Stage extends FlxGroup
 		}
 	}
 
+	public function reloadStage(curStage:String = "")
+	{
+		this.clear();
+		foreground.clear();
+		this.curStage = curStage;
+		
+		gfPos.set(660, 580);
+		dadPos.set(260, 700);
+		bfPos.set(1100, 700);
+		
+		if(scripted.contains(curStage))
+			callScript("create");
+		else
+			loadCode(curStage);
+
+		PlayState.defaultCamZoom = camZoom;
+	}
+
 	public function preloadScript(stage:String = "")
 	{
-		var path:String = 'images/backgrounds/_scripts/$stage.hxc';
+		var path:String = 'images/stages/_scripts/$stage.hxc';
 		
 		if(!Paths.fileExists(path))
 			return;
 
-		var scriptConfig:IrisConfig = new IrisConfig(path, true, true);
+		var scriptConfig:IrisConfig = new IrisConfig(path, false, true);
 		var newScript:Iris = new Iris(Paths.script('$path'), scriptConfig);
 
 		// variables to be used inside the scripts
+		newScript.set("FlxSprite", FlxSprite);
+		newScript.set("Paths", Paths);
 		newScript.set("this", instance);
-		newScript.set("add", this.add);
+
+		newScript.set("add", add);
 		newScript.set("foreground", foreground);
 
 		newScript.set("bfPos", bfPos);
@@ -84,153 +106,123 @@ class Stage extends FlxGroup
 		newScript.set("dadCam", dadCam);
 		newScript.set("gfCam", gfCam);
 
-		/* WORK IN PROGRESS!
-		newScript.set("curStage", curStage);
-		newScript.set("gfVersion", gfVersion);
-		newScript.set("defaultCamZoom", PlayState.defaultCamZoom);
-		*/
+		newScript.execute();
 
 		loadedScripts.push(newScript);
 		scripted.push(stage);
 	}
 
-	public function reloadStage(curStage:String = "")
+	// Hardcode your stages here!
+	public function loadCode(curStage:String = "")
 	{
-		this.clear();
-		foreground.clear();
-		
-		gfPos.set(660, 580);
-		/*dadPos.set(100,700);
-		bfPos.set(850, 700);*/
-		dadPos.set(260, 700);
-		bfPos.set(1100, 700);
-		
-		PlayState.defaultCamZoom = 1.0;
-		
-		this.curStage = curStage;
+		gfVersion = getGfVersion(curStage);
 
-		if(scripted.contains(curStage))
-			callScript("create");
-		else {
-			gfVersion = getGfVersion(curStage);
-
-			switch(curStage)
-			{
-				default:
-					this.curStage = "stage";
-					PlayState.defaultCamZoom = 0.9;
-					
-					var bg = new FlxSprite(-600, -600).loadGraphic(Paths.image("backgrounds/stage/stageback"));
-					bg.scrollFactor.set(0.6,0.6);
-					add(bg);
-					
-					var front = new FlxSprite(-580, 440);
-					front.loadGraphic(Paths.image("backgrounds/stage/stagefront"));
-					add(front);
-					
-					var curtains = new FlxSprite(-600, -400).loadGraphic(Paths.image("backgrounds/stage/stagecurtains"));
-					curtains.scrollFactor.set(1.4,1.4);
-					foreground.add(curtains);
+		switch(curStage)
+		{
+			default:
+				this.curStage = "stage";
+				camZoom = 0.9;
 				
-				/*
-				case "mugen":
-					PlayState.defaultCamZoom = 0.7;
-					//gfPos.y += 80;
-					dadPos.x -= 100;
-					
-					var bg = new FlxSprite(-640, -1000).loadGraphic(Paths.image("backgrounds/mugen/mugen"));
-					add(bg);
-				*/
-					
-				case "school":
-					bfPos.x -= 70;
-					dadPos.x += 50;
-					gfPos.x += 20;
-					gfPos.y += 50;
-					
-					var bgSky = new FlxSprite().loadGraphic(Paths.image('backgrounds/school/weebSky'));
-					bgSky.scrollFactor.set(0.1, 0.1);
-					add(bgSky);
-					
-					var bgSchool:FlxSprite = new FlxSprite(-200, 0).loadGraphic(Paths.image('backgrounds/school/weebSchool'));
-					bgSchool.scrollFactor.set(0.6, 0.90);
-					add(bgSchool);
-					
-					var bgStreet:FlxSprite = new FlxSprite(-200).loadGraphic(Paths.image('backgrounds/school/weebStreet'));
-					bgStreet.scrollFactor.set(0.95, 0.95);
-					add(bgStreet);
-					
-					var fgTrees:FlxSprite = new FlxSprite(-200 + 170, 130).loadGraphic(Paths.image('backgrounds/school/weebTreesBack'));
-					fgTrees.scrollFactor.set(0.9, 0.9);
-					add(fgTrees);
-					
-					var bgTrees:FlxSprite = new FlxSprite(-200 - 380, -1100);
-					bgTrees.frames = Paths.getPackerAtlas('backgrounds/school/weebTrees');
-					bgTrees.animation.add('treeLoop', CoolUtil.intArray(18), 12);
-					bgTrees.animation.play('treeLoop');
-					bgTrees.scrollFactor.set(0.85, 0.85);
-					add(bgTrees);
-					
-					var treeLeaves:FlxSprite = new FlxSprite(-200, -40);
-					treeLeaves.frames = Paths.getSparrowAtlas('backgrounds/school/petals');
-					treeLeaves.animation.addByPrefix('leaves', 'PETALS ALL', 24, true);
-					treeLeaves.animation.play('leaves');
-					treeLeaves.scrollFactor.set(0.85, 0.85);
-					add(treeLeaves);
-					
-					var bgGirls = new FlxSprite(-100, 175); // 190
-					bgGirls.frames = Paths.getSparrowAtlas('backgrounds/school/bgFreaks');
-					bgGirls.scrollFactor.set(0.9, 0.9);
-					
-					var girlAnim:String = "girls group";
-					if(PlayState.SONG.song == 'roses')
-						girlAnim = 'fangirls dissuaded';
-					
-					bgGirls.animation.addByIndices('danceLeft',  'BG $girlAnim', CoolUtil.intArray(14),		"", 24, false);
-					bgGirls.animation.addByIndices('danceRight', 'BG $girlAnim', CoolUtil.intArray(30, 15), "", 24, false);
-					bgGirls.animation.play('danceLeft');
-					bgGirls._stepHit = function(curStep:Int)
+				var bg = new FlxSprite(-600, -600).loadGraphic(Paths.image("stages/stage/stageback"));
+				bg.scrollFactor.set(0.6,0.6);
+				add(bg);
+				
+				var front = new FlxSprite(-580, 440);
+				front.loadGraphic(Paths.image("stages/stage/stagefront"));
+				add(front);
+				
+				var curtains = new FlxSprite(-600, -400).loadGraphic(Paths.image("stages/stage/stagecurtains"));
+				curtains.scrollFactor.set(1.4,1.4);
+				foreground.add(curtains);
+				
+			case "school":
+				bfPos.x -= 70;
+				dadPos.x += 50;
+				gfPos.x += 20;
+				gfPos.y += 50;
+				
+				var bgSky = new FlxSprite().loadGraphic(Paths.image('stages/school/weebSky'));
+				bgSky.scrollFactor.set(0.1, 0.1);
+				add(bgSky);
+				
+				var bgSchool:FlxSprite = new FlxSprite(-200, 0).loadGraphic(Paths.image('stages/school/weebSchool'));
+				bgSchool.scrollFactor.set(0.6, 0.90);
+				add(bgSchool);
+				
+				var bgStreet:FlxSprite = new FlxSprite(-200).loadGraphic(Paths.image('stages/school/weebStreet'));
+				bgStreet.scrollFactor.set(0.95, 0.95);
+				add(bgStreet);
+				
+				var fgTrees:FlxSprite = new FlxSprite(-200 + 170, 130).loadGraphic(Paths.image('stages/school/weebTreesBack'));
+				fgTrees.scrollFactor.set(0.9, 0.9);
+				add(fgTrees);
+				
+				var bgTrees:FlxSprite = new FlxSprite(-200 - 380, -1100);
+				bgTrees.frames = Paths.getPackerAtlas('stages/school/weebTrees');
+				bgTrees.animation.add('treeLoop', CoolUtil.intArray(18), 12);
+				bgTrees.animation.play('treeLoop');
+				bgTrees.scrollFactor.set(0.85, 0.85);
+				add(bgTrees);
+				
+				var treeLeaves:FlxSprite = new FlxSprite(-200, -40);
+				treeLeaves.frames = Paths.getSparrowAtlas('stages/school/petals');
+				treeLeaves.animation.addByPrefix('leaves', 'PETALS ALL', 24, true);
+				treeLeaves.animation.play('leaves');
+				treeLeaves.scrollFactor.set(0.85, 0.85);
+				add(treeLeaves);
+				
+				var bgGirls = new FlxSprite(-100, 175); // 190
+				bgGirls.frames = Paths.getSparrowAtlas('stages/school/bgFreaks');
+				bgGirls.scrollFactor.set(0.9, 0.9);
+				
+				var girlAnim:String = "girls group";
+				if(PlayState.SONG.song == 'roses')
+					girlAnim = 'fangirls dissuaded';
+				
+				bgGirls.animation.addByIndices('danceLeft',  'BG $girlAnim', CoolUtil.intArray(14),		"", 24, false);
+				bgGirls.animation.addByIndices('danceRight', 'BG $girlAnim', CoolUtil.intArray(30, 15), "", 24, false);
+				bgGirls.animation.play('danceLeft');
+				bgGirls._stepHit = function(curStep:Int)
+				{
+					if(curStep % 4 == 0)
 					{
-						if(curStep % 4 == 0)
-						{
-							if(bgGirls.animation.curAnim.name == 'danceLeft')
-								bgGirls.animation.play('danceRight', true);
-							else
-								bgGirls.animation.play('danceLeft', true);
-						}
+						if(bgGirls.animation.curAnim.name == 'danceLeft')
+							bgGirls.animation.play('danceRight', true);
+						else
+							bgGirls.animation.play('danceLeft', true);
 					}
-					add(bgGirls);
-					
-					// easier to manage
-					for(rawItem in members)
+				}
+				add(bgGirls);
+				
+				// easier to manage
+				for(rawItem in members)
+				{
+					if(Std.isOfType(rawItem, FlxSprite))
 					{
-						if(Std.isOfType(rawItem, FlxSprite))
-						{
-							var item:FlxSprite = cast rawItem;
-							item.antialiasing = false;
-							item.isPixelSprite = true;
-							item.scale.set(6,6);
-							item.updateHitbox();
-							item.x -= 170;
-							item.y -= 145;
-						}
+						var item:FlxSprite = cast rawItem;
+						item.antialiasing = false;
+						item.isPixelSprite = true;
+						item.scale.set(6,6);
+						item.updateHitbox();
+						item.x -= 170;
+						item.y -= 145;
 					}
-					
-				case "school-evil":
-					bfPos.x -= 70;
-					dadPos.x += 50;
-					gfPos.x += 20;
-					gfPos.y += 50;
-					
-					var bg:FlxSprite = new FlxSprite(400, 100);
-					bg.frames = Paths.getSparrowAtlas('backgrounds/school/animatedEvilSchool');
-					bg.animation.addByPrefix('idle', 'background 2', 24);
-					bg.animation.play('idle');
-					bg.scrollFactor.set(0.8, 0.9);
-					bg.antialiasing = false;
-					bg.scale.set(6,6);
-					add(bg);
-			}
+				}
+				
+			case "school-evil":
+				bfPos.x -= 70;
+				dadPos.x += 50;
+				gfPos.x += 20;
+				gfPos.y += 50;
+				
+				var bg:FlxSprite = new FlxSprite(400, 100);
+				bg.frames = Paths.getSparrowAtlas('stages/school/animatedEvilSchool');
+				bg.animation.addByPrefix('idle', 'background 2', 24);
+				bg.animation.play('idle');
+				bg.scrollFactor.set(0.8, 0.9);
+				bg.antialiasing = false;
+				bg.scale.set(6,6);
+				add(bg);
 		}
 	}
 
