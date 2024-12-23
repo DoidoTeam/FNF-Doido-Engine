7fea948a12bb025057589cae20a29551d58ef207
teles <85184617+iamteles@users.noreply.github.com>
2024-12-23 04:05:52 +0000

    credit coco for the custom buttons

--- source/states/menu/CreditsState.hx
+++ source/states/menu/CreditsState.hx
@@ -81,6 +81,7 @@ class CreditsState extends MusicBeatState
 		addCredit('JulianoBeta', 		'juyko', 	 0xFF0BA5FF, "Composed Doido Engine's offset menu music",			'https://www.youtube.com/@prodjuyko');
 		addCredit('crowplexus',			'crowplexus',0xFF313538, "Creator of HScript Iris",							'https://github.com/crowplexus/hscript-iris');
 		addCredit('yoisabo',			'yoisabo',	 0xFF56EF19, "Chart Editor's Event Icons Artist",					'https://bsky.app/profile/yoisabo.bsky.social');
+		addCredit('cocopuffs',			'coco',	 	 0xFF56EF19, "Mobile Button Artist",								'https://x.com/cocopuffswow');
 		addCredit('doubleonikoo', 		'nikoo', 	 0xFF60458A, "didn't really do much but i already made this icon so you can stay... for now\n-DiogoTV",	'https://bsky.app/profile/doubleonikoo.bsky.social');
 		addCredit('Github Contributors','github', 	 0xFFFFFFFF, 'Thank you\n${specialCoders}!!', 		'https://github.com/DoidoTeam/FNF-Doido-Engine/graphs/contributors');
 		addCredit('Special Thanks', 	'heart', 	 0xFFC01B42, 'Thank you\n${specialPeople}!!\n<33', "https://youtu.be/rnr8Oo2RV_c");
