875f2b4187fcd6c05f8bd5e202ee17bba6e5726c
teles <85184617+iamteles@users.noreply.github.com>
2024-12-24 00:07:20 +0000

    :bowtie:

--- source/states/WarningState.hx
+++ source/states/WarningState.hx
@@ -12,11 +12,11 @@ class WarningState extends MusicBeatState
 	override public function create():Void 
 	{
 		super.create();
-		var tex:String = "Warning!\n\n
-			This mod features flashing lights that may\n
-			be harmful to those with photosensitivity.\n
-			You can disable them in the Options menu.\n\n
-			Press ACCEPT to continue";
+		var tex:String = "Warning!\n\n"
+			+ "This mod features flashing lights that may\n"
+			+ "be harmful to those with photosensitivity.\n"
+			+ "You can disable them in the Options menu.\n\n"
+			+ "Press ACCEPT to continue";
 		var popUpTxt = new FlxText(0,0,0,tex);
 		popUpTxt.setFormat(Main.gFont, 36, 0xFFFFFFFF, CENTER);
 		popUpTxt.screenCenter();
