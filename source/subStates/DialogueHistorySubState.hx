6663d15989a35e3b9fcd469ce5886b5e6f367aff
teles <85184617+iamteles@users.noreply.github.com>
2024-12-25 20:37:16 +0000

    rename textlogsubstate to dialoguehistorysubstate

--- source/subStates/DialogueHistorySubState.hx
+++ source/subStates/DialogueHistorySubState.hx
@@ -17,7 +17,7 @@ import objects.menu.Alphabet;
 import objects.dialogue.DialogueObjects;
 import states.*;
 
-class TextLogSubState extends MusicBeatSubState
+class DialogueHistorySubState extends MusicBeatSubState
 {	
     var data:DialogueData;
 
