cbbf8af80145dbce48158e62c8ac9a5bdb86986a
teles <85184617+iamteles@users.noreply.github.com>
2026-02-21 03:09:57 +0000

    temporary freeplay + useless

--- source/states/DebugMenu.hx
+++ source/states/DebugMenu.hx
@@ -13,7 +13,7 @@ using doido.utils.TextUtil;
 
 class DebugMenu extends MusicBeatState
 {
-    var options:Array<String> = ["PlayState", "Controls", "Options", "Chart Converter"];
+    var options:Array<String> = ["Play", "Controls", "Options", "Chart Converter"];
     var text:FlxText;
     var title:FlxText;
     var ver:FlxText;
@@ -72,9 +72,81 @@ class DebugMenu extends MusicBeatState
                 case "chart converter":
                     MusicBeat.switchState(new ChartConverter());
                 default:
+                    MusicBeat.switchState(new Freeplay());
+                    /*
                     PlayState.loadSong("corn-theft", "hard");
                     MusicBeat.switchState(new states.PlayState());
+                    */
             }
+        }
+    }
+
+    public function changeSelection(change:Int = 0)
+	{
+		if(change != 0) FlxG.sound.play(Assets.sound('scroll'));
+		
+		cur += change;
+		cur = FlxMath.wrap(cur, 0, options.length - 1);
+		drawText();
+	}
+}
+
+class Freeplay extends MusicBeatState
+{
+    var options:Array<String> = ["bopeebo", "corn-theft", "useless"];
+    var text:FlxText;
+    var title:FlxText;
+    var cur:Int = 0;
+
+    override function create()
+    {
+        super.create();
+        DiscordIO.changePresence("In the Freeplay Menu");
+
+        var bg = new FlxSprite().loadGraphic(Assets.image('menuInvert'));
+		add(bg);
+
+        text = new FlxText(10, 0, 0, '');
+		text.setFormat(Main.globalFont, 48, 0xFFFFFFFF, LEFT);
+		text.setOutline(0xFF000000, 3);
+		add(text);
+        drawText();
+        text.y = FlxG.height - text.height - 10;
+
+        title = new FlxText(10, 0, 0, 'Freeplay');
+		title.setFormat(Main.globalFont, 100, 0xFFFFFFFF, LEFT);
+		title.setOutline(0xFF000000, 5);
+        title.y = text.y - title.height;
+		add(title);
+    }
+
+    function drawText() {
+        text.text = "";
+        for(i in 0...options.length)
+            text.text += options[i] + (i == cur ? " <\n" : "\n");
+    }
+
+    override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+        if(Controls.justPressed(UI_UP))
+            changeSelection(-1);
+        if(Controls.justPressed(UI_DOWN))
+            changeSelection(1);
+
+        if(Controls.justPressed(BACK))
+			MusicBeat.switchState(new states.DebugMenu());
+
+        if(Controls.justPressed(ACCEPT)) {
+            try {
+				PlayState.loadSong(options[cur], "hard");
+                MusicBeat.switchState(new states.PlayState());
+			}
+			catch(e) {
+				FlxG.sound.play(Assets.sound('beep'));
+			}
+            
         }
     }
 
@@ -111,18 +183,11 @@ class ChartConverter extends MusicBeatState
         drawText();
         text.y = FlxG.height - text.height - 10;
 
-        title = new FlxText(10, 0, 0, 'DE-Pudim');
+        title = new FlxText(10, 0, 0, "Chart Converter");
 		title.setFormat(Main.globalFont, 100, 0xFFFFFFFF, LEFT);
 		title.setOutline(0xFF000000, 5);
         title.y = text.y - title.height;
 		add(title);
-
-        ver = new FlxText(10, 0, 0, "Chart Converter");
-		ver.setFormat(Main.globalFont, 32, 0xFFFFFFFF, LEFT);
-		ver.setOutline(0xFF000000, 2.5);
-        ver.x = title.x + title.width + 5;
-        ver.y = text.y - ver.height;
-		add(ver);
     }
 
     function drawText() {
