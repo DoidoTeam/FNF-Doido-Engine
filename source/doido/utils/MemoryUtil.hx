9ccb825a2f8397610463cdd90a2d352beaba5a52
teles <85184617+iamteles@users.noreply.github.com>
2026-06-14 14:55:02 +0100

    organize

--- source/doido/utils/MemoryUtil.hx
+++ source/doido/utils/MemoryUtil.hx
@@ -1,33 +1,23 @@
 package doido.utils;
-import flixel.math.FlxMath;
-class MemoryUtil 
+
+class MemoryUtil
 {
+	public static var units:Array<String> = ["B", "KB", "MB", "GB", "TB", "PB"];
+
 	/**
 	 * Version of flixel's formatBytes function so a new array isnt created each call.
-	 * @param Bytes 
-	 * @param Precision 
+	 * @param bytes
 	 * @return String
 	 */
-	inline public static function formatMemory(Bytes:Float, Precision:Int = 2):String
+	inline public static function formatMemory(bytes:Float):String
 	{
 		var curUnit = 0;
-		while (Bytes >= 1024)
+		while (bytes >= 1024)
 		{
-			Bytes /= 1024;
+			bytes /= 1024;
 			curUnit++;
 		}
-	   
-	    return FlxMath.roundDecimal(Bytes, Precision) + getUnits(curUnit);
+		
+		return (Math.fround(bytes * 100) / 100) + units[curUnit];
 	}
-
-    inline private static function getUnits(unit:Int)
-    {
-        switch(unit)
-		{
-			case 0:  return "Bytes";
-			case 1: return  "KB";
-			case 2: return  "MB";
-			default: return  "GB";
-		};
-    }
 }
