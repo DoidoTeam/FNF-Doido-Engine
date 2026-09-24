7d0362977d94a3ce7e890c419aca6c45c16f6243
ShadzXD <136199318+ShadzXD@users.noreply.github.com>
2026-09-24 08:55:18 +0200

    maybe?

--- source/objects/Character.hx
+++ source/objects/Character.hx
@@ -81,11 +81,6 @@ class Character extends DoidoSprite
 
 	public function loadCharacter(reload:Bool = false)
 	{
-		if(curChar == 'none')
-		{
-			kill();
-		}
-
 		if (!reload)
 		{
 			try
@@ -98,18 +93,16 @@ class Character extends DoidoSprite
 				data = defaultCharacter();
 			}
 		}
-
-		spriteType = DoidoSprite.stringToSpriteType(data.spriteType);
-		atlasType = DoidoSprite.stringToAtlasType(data.atlasType);
-
-		var extrasheets:Array<String> = [];
-		if ((data.extrasheets ?? []).length > 0)
+		if(curChar != 'none')
 		{
-			for (sheet in (data.extrasheets ?? []))
-				extrasheets.push('images/$spritePath$sheet');
+			loadSprite();
 		}
-
-		frames = cast Assets.framesCollection('$spritePath${data.spritesheet}', extrasheets, spriteType);
+		else
+		{
+			kill();
+			return;
+		}
+		
 		for (animData in data.anims)
 			addAnim(animData);
 
@@ -145,6 +138,21 @@ class Character extends DoidoSprite
 		playAnim(idleAnims[0], true, idleFrames);
 	}
 
+	function loadSprite()
+	{
+		spriteType = DoidoSprite.stringToSpriteType(data.spriteType);
+		atlasType = DoidoSprite.stringToAtlasType(data.atlasType);
+
+		var extrasheets:Array<String> = [];
+		if ((data.extrasheets ?? []).length > 0)
+		{
+			for (sheet in (data.extrasheets ?? []))
+				extrasheets.push('images/$spritePath$sheet');
+		}
+
+		frames = cast Assets.framesCollection('$spritePath${data.spritesheet}', extrasheets, spriteType);
+	}
+	
 	public function singTypeFromString(type:Null<String>)
 	{
 		singType = switch ((type ?? "").toUpperCase())
